e5da2e963a34dacfcb82ca0139797398c2323713
nmathieu <nmathieu@student.42.fr>
2022-05-20 15:56:29 +0200

    better error messages

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: nmathieu <nmathieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 14:20:48 by nmathieu          #+#    #+#             */
-/*   Updated: 2022/05/19 19:31:18 by nmathieu         ###   ########.fr       */
+/*   Updated: 2022/05/20 15:56:17 by nmathieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,9 +39,9 @@ static bool	validate_map_path(const char *s)
 static void	print_perr(t_perr err, t_map_parser *parser)
 {
 	if (err == SL_PERR_CANT_READ)
-		ft_fmt_err("Read Error\n");
+		ft_fmt_err("An error occured whilst reading the file.\n");
 	if (err == SL_PERR_OUT_OF_MEMORY)
-		ft_fmt_err("Out Of Memory\n");
+		ft_fmt_err("The system is out of memory.\n");
 	if (err == SL_PERR_INVALID_MAP)
 		sl_print_map_error(parser);
 }
@@ -49,13 +49,13 @@ static void	print_perr(t_perr err, t_map_parser *parser)
 static void	print_gerr(t_gerr err)
 {
 	if (err == SL_GERR_MLX)
-		ft_fmt_err("MiniLibX Error\n");
+		ft_fmt_err("Failed to initialize the MiniLibX.\n");
 	if (err == SL_GERR_IMAGE)
-		ft_fmt_err("Unsupported image/X11 server\n");
+		ft_fmt_err("Unsupported image/X11 server.\n");
 	if (err == SL_GERR_OOM)
-		ft_fmt_err("Out Of Memory\n");
+		ft_fmt_err("The system is out of memory.\n");
 	if (err == SL_GERR_RNG)
-		ft_fmt_err("Can't read /dev/urandom\n");
+		ft_fmt_err("Cannot read '/dev/urandom'.\n");
 }
 
 int	main(int argc, char **argv)
@@ -66,12 +66,12 @@ int	main(int argc, char **argv)
 	t_gerr			gerr;
 
 	if (argc != 2)
-		return (ft_fmt_err("Usage: ./so_long <map_file>\n"), 1);
+		return (ft_fmt_err("Usage: ./so_long <map_file.ber>\n"), 1);
 	if (!validate_map_path(argv[1]))
-		return (ft_fmt_err("Maps must be .ber files\n"), 1);
+		return (ft_fmt_err("Maps must be .ber files.\n"), 1);
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
-		return (ft_fmt_err("Open Error\n"), 1);
+		return (ft_fmt_err("Cannot open '{s?}' for reading.\n", argv[1]), 1);
 	perr = sl_parse_map(fd, &p);
 	close(fd);
 	if (perr != SL_PERR_SUCCESS)
